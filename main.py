c5ab94ec3337c1e47a2d59b0d8ece40ac88aefa4
mohamad0880218 <115563288+mohamad0880218@users.noreply.github.com>
2023-09-28 11:54:24 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,6 @@ from src.predict import predict_image
 
 # Call the predict_image function with your image data
 # Replace 'image_path' with the path to your image file
-image_path = './Images/image.jpg'
+image_path = './Images/image.jpeg'
 predicted_class = predict_image(image_path)
 print(f"Predicted class: {predicted_class}")
