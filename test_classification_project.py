e6d4131525695cae430b50c2fd914ab91aad7368
mohamad0880218 <115563288+mohamad0880218@users.noreply.github.com>
2023-09-28 12:09:50 -0400

    Update test_classification_project.py

--- test_classification_project.py
+++ test_classification_project.py
@@ -1,10 +1,10 @@
 import pytest
 import tensorflow as tf
 import numpy as np
-from src.data_preparation import load_cifar10_data
-from src.model import create_cnn_model
-from src.train import train_model
-from src.predict import predict_image
+from ./src.data_preparation import load_cifar10_data
+from ./src.model import create_cnn_model
+from ./src.train import train_model
+from ./src.predict import predict_image
 
 @pytest.fixture
 def cifar10_data():
